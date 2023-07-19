28a851d6de89d50ab3a6f7ee34b61a62f078914e
Kimyeonho <kyh0703@nate.com>
2023-07-19 23:00:47 +0900

    [Feature] Story Book
    npx sb@latest init
    npm run story book

---
+++ stories/SytledButton.stories.tsx
@@ -0,0 +1,77 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { StyledButton } from '../components/StyledButton';
+import { action } from '@storybook/addon-actions';
+import { useState } from 'react';
+
+const meta: Meta = {
+  title: 'StyledButton',
+  component: StyledButton,
+  argTypes: {
+    variant: {
+      control: { type: 'radio' },
+      options: ['primary', 'success', 'transparent'],
+    },
+    children: {
+      control: { type: 'text' },
+    },
+    onClick: { action: 'clicked' },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof StyledButton>;
+
+export const Primary: Story = {
+  render: (props) => (
+    <StyledButton {...props} variant='primary'>
+      Primary
+    </StyledButton>
+  ),
+};
+
+export const Success: Story = {
+  render: (props) => (
+    <StyledButton {...props} variant='success'>
+      Success
+    </StyledButton>
+  ),
+};
+
+export const Transparent: Story = {
+  render: (props) => (
+    <StyledButton {...props} variant='transparent'>
+      Transparent
+    </StyledButton>
+  ),
+};
+
+const increnmentAction = action('increment');
+
+export const PrimaryWithAction: Story = {
+  render: (props) => {
+    const [count, setCount] = useState(0);
+
+    const onClick = (e: React.MouseEvent) => {
+      increnmentAction(e, count);
+      setCount((c) => c + 1);
+    };
+
+    return (
+      <StyledButton {...props} variant='primary' onClick={onClick}>
+        Primary
+      </StyledButton>
+    );
+  },
+};
+
+// const Template: Story = {
+//   render: (props) => <StyledButton {...props} />,
+// };
+
+// export const TemplateTest = Template.Bind({});
+
+// TemplateTest.args = {
+//   variant: 'primary',
+//   children: 'primary',
+// };
